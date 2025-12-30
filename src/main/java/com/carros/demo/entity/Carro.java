15bfb1f2e664e86c2191ebf89edde03c1bfb6537
MatheusLucas01 <matheuslucasdesouza22@gmail.com>
2025-12-30 11:32:55 -0300

    feat: Cria classe de entidade com getters and setters e atributos do carro

---
+++ src/main/java/com/carros/demo/entity/Carro.java
@@ -0,0 +1,46 @@
+package com.carros.demo.entity;
+
+public class Carro {
+
+    private String nome;
+    private String marca;
+    private String modelo;
+    private String ano;
+
+    // Getter and Setters dos atributos
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getMarca() {
+        return marca;
+    }
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+    public String getModelo() {
+        return modelo;
+    }
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+    public String getAno() {
+        return ano;
+    }
+    public void setAno(String ano) {
+        this.ano = ano;
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
